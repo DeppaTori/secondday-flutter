016bcef657b5b58f71f7b85f463265d1ea12a3e0
Tommy Toban <tommy.deppatori@gmail.com>
2020-01-08 14:52:24 +0700

    set system ui overlay style

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:mysecondflutterapp/SearchPage.dart';
 import 'package:mysecondflutterapp/Wallpapers.dart';
 
 import 'DartObject.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent
+  ));
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
